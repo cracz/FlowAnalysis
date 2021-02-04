f0b116ea8280a684c45d90b6865af07a6c97134d
Cameron Racz <cracz@bnl.gov>
2021-02-04 00:15:42 -0500

    Nothing important

--- postAnalysis/postAnalysis.sh
+++ postAnalysis/postAnalysis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-jobID="ellipticAutocorrAlgorithm"
+jobID="17221A3F07C531B0F551F6B531CBAC50"
 order_n="2"
 
-root -l -b -q plotAll.cxx\(\"${jobID}\"\)
+#root -l -b -q plotAll.cxx\(\"${jobID}\"\)
 #root -l -b -q m2BypTBins.cxx\(\"${jobID}\"\)
 #root -l -b -q yVsEtaPlots.cxx\(\"${jobID}\"\)
 #root -l -b -q acceptanceCuts.cxx\(\"${jobID}\"\)
@@ -15,5 +15,5 @@ root -l -b -q plotAll.cxx\(\"${jobID}\"\)
 #root -l -b -q overlay.cxx\(\"${jobID}\",\"E\"\)
 
 # EP Resolution and Flow Calculations
-#root -l -b -q resolutions.cxx\(\"${jobID}\",\"${order_n}\"\)
+root -l -b -q resolutions.cxx\(\"${jobID}\",\"${order_n}\"\)
 #root -l -b -q coefficients.cxx\(\"${jobID}\",\"${order_n}\"\)
