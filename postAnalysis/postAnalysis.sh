edf01cacb258daa930287bdd56ac0ad739a4e9aa
Cameron Racz <cracz001@ucr.edu>
2021-03-07 04:57:18 -0500

    Unimportant commit, fixing merge conflict.

--- postAnalysis/postAnalysis.sh
+++ postAnalysis/postAnalysis.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-jobID="ellipticAutocorrAlgorithm"
-order_n="2"
+#jobID="399BE5B48875569A02CB478AE7411FE5"
+jobID="triangularResults"
+order_n="3"
 
-root -l -b -q plotAll.cxx\(\"${jobID}\"\)
+#root -l -b -q plotAll.cxx\(\"${jobID}\"\)
 #root -l -b -q m2BypTBins.cxx\(\"${jobID}\"\)
 #root -l -b -q yVsEtaPlots.cxx\(\"${jobID}\"\)
 #root -l -b -q acceptanceCuts.cxx\(\"${jobID}\"\)
@@ -15,5 +16,5 @@ root -l -b -q plotAll.cxx\(\"${jobID}\"\)
 #root -l -b -q overlay.cxx\(\"${jobID}\",\"E\"\)
 
 # EP Resolution and Flow Calculations
-#root -l -b -q resolutions.cxx\(\"${jobID}\",\"${order_n}\"\)
-#root -l -b -q coefficients.cxx\(\"${jobID}\",\"${order_n}\"\)
+root -l -b -q resolutions.cxx\(\"${jobID}\",\"${order_n}\"\)
+root -l -b -q coefficients.cxx\(\"${jobID}\",\"${order_n}\"\)
