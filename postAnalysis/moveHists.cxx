ac1a28beb3c927de188aeef306f692721990e419
Cameron Racz <cracz001@ucr.edu>
2021-08-04 13:15:16 -0400

    I don't remember what changed here, but saving it just in case.

--- postAnalysis/moveHists.cxx
+++ postAnalysis/moveHists.cxx
@@ -1,4 +1,4 @@
-void moveHists(TString fileName = "42633A854769565B1841308ED4AA987C.picoDst.result.combined.root")
+void moveHists(TString fileName = "A30730E2E9613CAA96F5A362F308DB06.picoDst.result.combined.root")
 {
   TFile *file = TFile::Open(fileName);
 
@@ -9,30 +9,43 @@ void moveHists(TString fileName = "42633A854769565B1841308ED4AA987C.picoDst.resu
   TProfile *p_vn_kp = (TProfile*)file->Get("p_vn_kp");
   TProfile *p_vn_km = (TProfile*)file->Get("p_vn_km");
   TProfile *p_vn_pr = (TProfile*)file->Get("p_vn_pr");
-  /*
-  TProfile *p_vn_yCM_00to10_pp = (TProfile*)file->Get("p_vn_yCM_00to10_pp");
-  TProfile *p_vn_yCM_10to40_pp = (TProfile*)file->Get("p_vn_yCM_10to40_pp");
-  TProfile *p_vn_yCM_40to60_pp = (TProfile*)file->Get("p_vn_yCM_40to60_pp");
 
-  TProfile *p_vn_yCM_00to10_pm = (TProfile*)file->Get("p_vn_yCM_00to10_pm");
-  TProfile *p_vn_yCM_10to40_pm = (TProfile*)file->Get("p_vn_yCM_10to40_pm");
-  TProfile *p_vn_yCM_40to60_pm = (TProfile*)file->Get("p_vn_yCM_40to60_pm");
+  TProfile2D *p2_vn_yCM_cent_pp = (TProfile2D*)file->Get("p2_vn_yCM_cent_pp");
+  TProfile2D *p2_vn_yCM_cent_pm = (TProfile2D*)file->Get("p2_vn_yCM_cent_pm");
+  TProfile2D *p2_vn_yCM_cent_kp = (TProfile2D*)file->Get("p2_vn_yCM_cent_kp");
+  TProfile2D *p2_vn_yCM_cent_km = (TProfile2D*)file->Get("p2_vn_yCM_cent_km");
+  TProfile2D *p2_vn_yCM_cent_pr = (TProfile2D*)file->Get("p2_vn_yCM_cent_pr");
+  TProfile2D *p2_vn_yCM_cent_pr_symmetry = (TProfile2D*)file->Get("p2_vn_yCM_cent_pr_symmetry");
+
+  p2_vn_yCM_cent_kp->RebinY();
+  p2_vn_yCM_cent_km->RebinY();
+
+  TProfile *p_vn_yCM_00to10_pp = p2_vn_yCM_cent_pp->ProfileY("p_vn_yCM_00to10_pp", 15, 16);
+  TProfile *p_vn_yCM_10to40_pp = p2_vn_yCM_cent_pp->ProfileY("p_vn_yCM_10to40_pp", 9, 14);
+  TProfile *p_vn_yCM_40to60_pp = p2_vn_yCM_cent_pp->ProfileY("p_vn_yCM_40to60_pp", 5, 8);
   
-  TProfile *p_vn_yCM_00to10_kp = (TProfile*)file->Get("p_vn_yCM_00to10_kp");
-  TProfile *p_vn_yCM_10to40_kp = (TProfile*)file->Get("p_vn_yCM_10to40_kp");
-  TProfile *p_vn_yCM_40to60_kp = (TProfile*)file->Get("p_vn_yCM_40to60_kp");
+  TProfile *p_vn_yCM_00to10_pm = p2_vn_yCM_cent_pm->ProfileY("p_vn_yCM_00to10_pm", 15, 16);
+  TProfile *p_vn_yCM_10to40_pm = p2_vn_yCM_cent_pm->ProfileY("p_vn_yCM_10to40_pm", 9, 14);
+  TProfile *p_vn_yCM_40to60_pm = p2_vn_yCM_cent_pm->ProfileY("p_vn_yCM_40to60_pm", 5, 8);
+
+  TProfile *p_vn_yCM_00to10_kp = p2_vn_yCM_cent_kp->ProfileY("p_vn_yCM_00to10_kp", 15, 16);
+  TProfile *p_vn_yCM_10to40_kp = p2_vn_yCM_cent_kp->ProfileY("p_vn_yCM_10to40_kp", 9, 14);
+  TProfile *p_vn_yCM_40to60_kp = p2_vn_yCM_cent_kp->ProfileY("p_vn_yCM_40to60_kp", 5, 8);
+
+  TProfile *p_vn_yCM_00to10_km = p2_vn_yCM_cent_km->ProfileY("p_vn_yCM_00to10_km", 15, 16);
+  TProfile *p_vn_yCM_10to40_km = p2_vn_yCM_cent_km->ProfileY("p_vn_yCM_10to40_km", 9, 14);
+  TProfile *p_vn_yCM_40to60_km = p2_vn_yCM_cent_km->ProfileY("p_vn_yCM_40to60_km", 5, 8);
+
+  TProfile *p_vn_yCM_00to10_pr = p2_vn_yCM_cent_pr->ProfileY("p_vn_yCM_00to10_pr", 15, 16);
+  TProfile *p_vn_yCM_10to40_pr = p2_vn_yCM_cent_pr->ProfileY("p_vn_yCM_10to40_pr", 9, 14);
+  TProfile *p_vn_yCM_40to60_pr = p2_vn_yCM_cent_pr->ProfileY("p_vn_yCM_40to60_pr", 5, 8);
+
+  TProfile *p_vn_yCM_00to10_pr_symm = p2_vn_yCM_cent_pr_symmetry->ProfileY("p_vn_yCM_00to10_pr_symm", 15, 16);
+  TProfile *p_vn_yCM_10to40_pr_symm = p2_vn_yCM_cent_pr_symmetry->ProfileY("p_vn_yCM_10to40_pr_symm", 9, 14);
+  TProfile *p_vn_yCM_40to60_pr_symm = p2_vn_yCM_cent_pr_symmetry->ProfileY("p_vn_yCM_40to60_pr_symm", 5, 8);
+
   
-  TProfile *p_vn_yCM_00to10_km = (TProfile*)file->Get("p_vn_yCM_00to10_km");
-  TProfile *p_vn_yCM_10to40_km = (TProfile*)file->Get("p_vn_yCM_10to40_km");
-  TProfile *p_vn_yCM_40to60_km = (TProfile*)file->Get("p_vn_yCM_40to60_km");
-  
-  TProfile *p_vn_yCM_00to10_pr = (TProfile*)file->Get("p_vn_yCM_00to10_pr");
-  TProfile *p_vn_yCM_10to40_pr = (TProfile*)file->Get("p_vn_yCM_10to40_pr");
-  TProfile *p_vn_yCM_40to60_pr = (TProfile*)file->Get("p_vn_yCM_40to60_pr");
-  */
-  
-  //TFile *newFile = new TFile("v3_vs_yCM.root", "RECREATE");
-  TFile *newFile = new TFile("v2_vs_cent.root", "RECREATE");
+  TFile *newFile = new TFile("v3_vs_yCM.root", "RECREATE");
   newFile->cd();
 
   p_vn_Tpc_pT_0p2to2->Write();
@@ -42,8 +55,6 @@ void moveHists(TString fileName = "42633A854769565B1841308ED4AA987C.picoDst.resu
   p_vn_km->Write();
   p_vn_pr->Write();
 
-  
-  /*
   p_vn_yCM_00to10_pp->Write();
   p_vn_yCM_10to40_pp->Write();
   p_vn_yCM_40to60_pp->Write();
@@ -63,7 +74,11 @@ void moveHists(TString fileName = "42633A854769565B1841308ED4AA987C.picoDst.resu
   p_vn_yCM_00to10_pr->Write();
   p_vn_yCM_10to40_pr->Write();
   p_vn_yCM_40to60_pr->Write();
-  */
+
+  p_vn_yCM_00to10_pr_symm->Write();
+  p_vn_yCM_10to40_pr_symm->Write();
+  p_vn_yCM_40to60_pr_symm->Write();
+
 
   file->Close();
   newFile->Close();
